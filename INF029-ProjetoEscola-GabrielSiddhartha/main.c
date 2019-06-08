1419aa3206d076a4c22845b1548feeaa619a567d
gabriel siddhartha <gab_siddhartha@hotmail.com>
2019-06-08 10:32:25 -0300

    adicao de fflush e funcao

--- INF029-ProjetoEscola-GabrielSiddhartha/main.c
+++ INF029-ProjetoEscola-GabrielSiddhartha/main.c
@@ -1,70 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
+#define TAM_ALUNOS 2
 
 typedef struct ficha_aluno{
 	char nome[100], sexo, dnascimento[10], cpf[11];
 	int idade, x, y;
 } Aluno;
+
 void flush_in(){
 	int ch;
 	do{
 		ch = fgetc(stdin);
-
 	}while (ch != EOF && ch !='\n');
 }
 
 
 int main() {
 
+    char cpf2[100];
+    char achou='F';
+    int idade, x, y,z;
+    Aluno lista_alunos[TAM_ALUNOS];
 
-  char cpf2[100];
-  char achou='F';
-  int idade, x, y,z;
-  Aluno lista_alunos[10];
-
-  for( x=0; x<1; x++)
+    for( x=0; x < TAM_ALUNOS; x++)
 	{
-		printf(" nome:\n");
-		//flush_in();
+        printf("Nome: ");
 		gets(lista_alunos[x].nome);
-		//flush_in();
-		printf(" data de nascimento:\n");
 
+		printf("Data de nascimento: ");
 		gets(lista_alunos[x].dnascimento);
-		printf(" sexo, se homem= H, se mulher=M, se gay=G \n");
 
+		printf("Sexo, se homem= H, se mulher=M, se gay=G: ");
 		scanf("%c",&lista_alunos[x].sexo);
 		flush_in();
 
-		printf(" cpf:\n");
+		printf("Cpf:");
 		gets(lista_alunos[x].cpf);
 
-		printf(" idade:\n ");
+		printf("Idade: ");
 		scanf(" %d", &lista_alunos[x].idade);
+		getchar();
 
 	}
 
+	imprimir_alunos(lista_alunos);
 
-
-	//for(z=0;z<2;z++){
-	printf ("Informe o cpf: ");
-	gets (cpf2);
-
-	for( y=0;y<10;y++){
-
-	if (strcmp(cpf2,lista_alunos[y].cpf)==0)
-	{
-		printf(" nome: %s\n", lista_alunos[y].nome);
-		printf(" data de nacimento %s\n", lista_alunos[y].dnascimento);
-		printf(" sexo do aluno: %s\n", lista_alunos[y].sexo);
-		printf(" cpf: %s\n", lista_alunos[y].cpf);
-		printf("idade: %d\n", lista_alunos[y].idade);
-		achou='V';
-		if(achou=='F')
-			printf("usuario nao encontrado");
-		}
-
-	}
  	return 0;
+}
+
+void imprimir_alunos(Aluno lista_alunos[]){
+
+    int y;
+	for( y=0; y < TAM_ALUNOS; y++){
+
+        printf("Nome: %s\n", lista_alunos[y].nome);
+        printf("Data de nacimento %s\n", lista_alunos[y].dnascimento);
+        printf("Sexo do aluno: %c\n", lista_alunos[y].sexo);
+        printf("Cpf: %s\n", lista_alunos[y].cpf);
+        printf("Idade: %d\n", lista_alunos[y].idade);
+
+
+	}
+
 }
