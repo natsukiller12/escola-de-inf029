1f21e8286905814f091d44040210f1d0cf01bc55
gabriel siddhartha <gab_siddhartha@hotmail.com>
2019-06-08 10:16:26 -0300

    novo projeto codeblocks

--- INF029-ProjetoEscola-GabrielSiddhartha/main.c
+++ INF029-ProjetoEscola-GabrielSiddhartha/main.c
@@ -2,42 +2,52 @@
 #include <stdlib.h>
 #include<string.h>
 
-typedef struct ficha_aluno{	
+typedef struct ficha_aluno{
 	char nome[100], sexo, dnascimento[10], cpf[11];
 	int idade, x, y;
 } Aluno;
+void flush_in(){
+	int ch;
+	do{
+		ch = fgetc(stdin);
+
+	}while (ch != EOF && ch !='\n');
+}
 
 
 int main() {
-	
-  
+
+
   char cpf2[100];
   char achou='F';
   int idade, x, y,z;
   Aluno lista_alunos[10];
-  
+
   for( x=0; x<1; x++)
 	{
 		printf(" nome:\n");
-		fflush(stdin);
+		//flush_in();
 		gets(lista_alunos[x].nome);
+		//flush_in();
 		printf(" data de nascimento:\n");
-		fflush(stdin);
+
 		gets(lista_alunos[x].dnascimento);
 		printf(" sexo, se homem= H, se mulher=M, se gay=G \n");
-		fflush(stdin);
+
 		scanf("%c",&lista_alunos[x].sexo);
+		flush_in();
+
 		printf(" cpf:\n");
-		fflush(stdin);
 		gets(lista_alunos[x].cpf);
+
 		printf(" idade:\n ");
 		scanf(" %d", &lista_alunos[x].idade);
-		fflush(stdin);
+
 	}
-    
-	
-	
-	for(z=0;z<2;z++){
+
+
+
+	//for(z=0;z<2;z++){
 	printf ("Informe o cpf: ");
 	gets (cpf2);
 
@@ -45,24 +55,16 @@ int main() {
 
 	if (strcmp(cpf2,lista_alunos[y].cpf)==0)
 	{
-	 printf(" nome: %s\n", lista_alunos[y].nome);
-		fflush(stdin);
+		printf(" nome: %s\n", lista_alunos[y].nome);
 		printf(" data de nacimento %s\n", lista_alunos[y].dnascimento);
-		fflush(stdin);
 		printf(" sexo do aluno: %s\n", lista_alunos[y].sexo);
-		fflush(stdin);
 		printf(" cpf: %s\n", lista_alunos[y].cpf);
-		fflush(stdin);
 		printf("idade: %d\n", lista_alunos[y].idade);
 		achou='V';
-		
-	
-	
-	
-	
-	if(achou=='F')
-	printf("usuario nao encontrado");	}
+		if(achou=='F')
+			printf("usuario nao encontrado");
+		}
 
-}
-} return 0;
+	}
+ 	return 0;
 }
